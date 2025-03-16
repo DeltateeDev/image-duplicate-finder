90ceb73c87e9e948d9e224d672249960560f75b1
Deltatee <14inin@googlemail.com>
2025-03-16 02:23:59 +0100

    Refactored the code to enchance performance

--- main.py
+++ main.py
@@ -1,40 +1,49 @@
 from PIL import Image
 import imagehash
 import os
+import concurrent.futures
 
 def calculate_hash(image_path):
     try:
         img = Image.open(image_path)
         hash = imagehash.dhash(img)
+        img.close()
         return hash
-    except (OSError, FileNotFoundError):
-        print(f"Error: Could not open or process image at {image_path}")
+    except (OSError, FileNotFoundError) as e:
+        print(f"Error: Could not open or process image at {image_path}: {e}")
         return None
 
 def find_duplicate_images(directory, threshold = 5):
+    image_paths = []
     hashes = {}
     duplicates = []
 
     for filename in os.listdir(directory):
-        if not filename.lower().endswith(('.png', '.jpg', 'jpeg', 'gif', '.bmp')):
-            continue
+        if filename.lower().endswith(('.png', '.jpg', 'jpeg', 'gif', '.bmp')):
+            image_paths.append(os.path.join(directory, filename))
+    
+    usable_cpu = max(1, os.cpu_count() - 2)
 
-        image_path = os.path.join(directory, filename)
-        hash_value = calculate_hash(image_path)
-        duplicate_found = False #a flag for not overwriting the existing file paths in hashes dictionary
+    with concurrent.futures.ProcessPoolExecutor(max_workers=usable_cpu) as executor:
+        hash_results = list(executor.map(calculate_hash, image_paths))
 
-        if hash_value is not None:
-            if not hashes:
-                hashes[hash_value] = image_path
+    for i, image_path in enumerate(image_paths):
+        hash_value = hash_results[i]
+        print(i)
+
+        if hash_value is None:
                 continue
-            for existing_hash, existing_image_path in list(hashes.items()):
+        
+        for existing_hash, existing_image_path in list(hashes.items()):
+            if (existing_hash - hash_value) < threshold:
+                duplicates.append({
+                    'Duplicate File': image_path, 
+                    'Original File': existing_image_path, 
+                    'Difference Score': (existing_hash - hash_value)
+                })
+                break
 
-                if (existing_hash - hash_value) < threshold:
-                    duplicate_found = True
-                    duplicates.append({'Duplicate File': image_path, 'Original File': existing_image_path, 'Difference Score': (existing_hash - hash_value)})
-
-            if not duplicate_found:
-                hashes[hash_value] = image_path
+        hashes[hash_value] = image_path
 
     return duplicates
 
@@ -44,8 +53,6 @@ if __name__ == "__main__":
     duplicates = find_duplicate_images(directory_to_scan)
 
     if duplicates:
-        # for value_list in duplicates.values():
-        #     print(value_list)
         for i in duplicates:
             print(i)
 
